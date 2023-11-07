c9a5387ca9189818a13fa87f68a7de45d666250e
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-11-07 11:32:52 -0600

    Lifecycle Methods in React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,20 +1,39 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
+      loading: false,
     };
   }
+  componentDidUpdate() {
+    console.log("Actualización");
+
+    if (!!this.state.loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validación");
+
+        this.setState({ loading: false });
+
+        console.log("Terminando la validación");
+      }, 3000);
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escribe el código de seguridad</p>
+
         {this.state.error && <p>Error: el código es incorrecto</p>}
+        {this.state.loading && <Loading />}
+
         <input placeholder="Código de seguridad" />
-        <button onClick={() => this.setState({ error: !this.state.error })}>
+        <button onClick={() => this.setState({ loading: true })}>
           Comprobar
         </button>
       </div>
