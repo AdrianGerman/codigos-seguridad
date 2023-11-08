8dc164285930ee86d5f222d16cb2800fb962c798
AdrianGerman <adrian.german1019@gmail.com>
2023-11-08 10:24:22 -0600

    Imperative states with useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { UseState } from "./UseState.js";
+// import { UseState } from "./UseState.js";
 import { UseStateHook } from "./UseStateHook.js";
 import { ClassState } from "./ClassState.js";
 
@@ -7,9 +7,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <UseState name="Use State" />
-      <UseStateHook name="Use State con Hooks" />
-      <ClassState name="Class State" />
+      {/* <UseState name="Use State" /> */}
+      <UseStateHook name="UseState" />
+      <ClassState name="ClassState" />
     </div>
   );
 }
--- src/UseStateHook.js
+++ src/UseStateHook.js
@@ -7,6 +7,8 @@ function UseStateHook({ name }) {
     value: "",
     error: false,
     loading: false,
+    deleted: false,
+    confirmed: false,
   });
 
   console.log(state.value);
@@ -22,6 +24,7 @@ function UseStateHook({ name }) {
           setState({
             ...state,
             loading: false,
+            confirmed: true,
           });
         } else {
           setState({
@@ -39,37 +42,90 @@ function UseStateHook({ name }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.loading]);
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>Por favor, escribe el código de seguridad</p>
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>Por favor, escribe el código de seguridad</p>
 
-      {state.error && !state.loading && <p>Error: el código es incorrecto</p>}
-      {state.loading && <p>Cargando...</p>}
+        {state.error && !state.loading && <p>Error: el código es incorrecto</p>}
+        {state.loading && <p>Cargando...</p>}
 
-      <input
-        value={state.value}
-        placeholder="Código de seguridad"
-        onChange={(event) => {
-          setState({
-            ...state,
-            value: event.target.value,
-          });
-        }}
-      />
-      <button
-        onClick={() => {
-          setState({
-            ...state,
-            loading: true,
-            error: false,
-          });
-        }}
-      >
-        Comprobar
-      </button>
-    </div>
-  );
+        <input
+          value={state.value}
+          placeholder="Código de seguridad"
+          onChange={(event) => {
+            setState({
+              ...state,
+              value: event.target.value,
+            });
+          }}
+        />
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              loading: true,
+              error: false,
+              value: "",
+            });
+          }}
+        >
+          Comprobar
+        </button>
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <React.Fragment>
+        <h2>Eliminar {name}</h2>
+
+        <p>¿Seguro que quieres eliminar UseState?</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              deleted: true,
+              value: "",
+            });
+          }}
+        >
+          SI, eliminar
+        </button>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              value: "",
+            });
+          }}
+        >
+          NO, regresar
+        </button>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h2>{name} fue eliminado</h2>
+
+        <p>SE HA ELIMINADO CORRECTAMENTE</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              deleted: false,
+              value: "",
+            });
+          }}
+        >
+          Recuperar {name}
+        </button>
+      </React.Fragment>
+    );
+  }
 }
 
 export { UseStateHook };
